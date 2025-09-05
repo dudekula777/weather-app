628ef940c096eed9f15dbfd04ad827f5ff17ce2f
dastagiri9990 <dastagiri9990@gmail.com>
2025-09-05 23:31:01 +0530

    micube

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,29 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "weather-app-s3-buscket"
-    key           = "weather-app/terraform.tfstate"
-    region        = "us-east-1"
-    use_lockfile  = true
+    bucket = "weather-app-s3-buscket"
+    key    = "weather-app/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"  # or whichever region you want
+  region = "us-east-1"
+}
+
+# Optional: create key pair in AWS
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-ec2-key"
+  public_key = file("~/.ssh/id_rsa.pub") # replace with your public key path
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Ubuntu official owner
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-}
-
-resource "aws_instance" "minikube" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.medium"
-  key_name = "my-ec2-key"
-  tags = {
-    Name = "minikube-server"
   }
 }
 
@@ -57,7 +54,18 @@ resource "aws_security_group" "minikube_sg" {
   }
 }
 
+resource "aws_instance" "minikube" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  key_name               = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.minikube_sg.id]
+
+  tags = {
+    Name = "minikube-server"
+  }
+}
+
 output "instance_public_ip" {
-  value = aws_instance.minikube.public_ip
+  value       = aws_instance.minikube.public_ip
   description = "Public IP of the Minikube EC2 instance"
 }
