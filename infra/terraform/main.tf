9a11fe9d0e5de137be5b4170f404fabf277f9860
dastagiri9990 <dastagiri9990@gmail.com>
2025-09-07 02:26:07 +0530

    deploy docker images

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -12,15 +12,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Reuse existing key pair (do not create new one)
+# Use existing key pair instead of creating a new one
 data "aws_key_pair" "my_key" {
   key_name = "my-ec2-key"
 }
 
-# Get latest Ubuntu 20.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"] # Ubuntu official owner
 
   filter {
     name   = "name"
@@ -28,7 +27,6 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Security group for Minikube
 resource "aws_security_group" "minikube_sg" {
   name        = "minikube-sg"
   description = "Allow Kubernetes traffic"
@@ -55,11 +53,10 @@ resource "aws_security_group" "minikube_sg" {
   }
 }
 
-# EC2 instance for Minikube
 resource "aws_instance" "minikube" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.medium"
-  key_name               = data.aws_key_pair.my_key.key_name   # ✅ fixed
+  key_name               = data.aws_key_pair.my_key.key_name
   vpc_security_group_ids = [aws_security_group.minikube_sg.id]
 
   tags = {
@@ -67,7 +64,6 @@ resource "aws_instance" "minikube" {
   }
 }
 
-# Output EC2 public IP
 output "instance_public_ip" {
   value       = aws_instance.minikube.public_ip
   description = "Public IP of the Minikube EC2 instance"
